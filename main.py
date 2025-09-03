fcea53fa35cc5442f1c2900ce8030f557733fb60
suazojaime <suazojaime@gmail.com>
2025-09-03 09:27:34 -0400

    including temperature monitoring

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ importlib.reload(telegram_api)
 
 if __name__ == '__main__':
     
-    [battery_status, power_input] = eco_values.get_ecoflow_values()
+    [battery_status, power_input, battery_temp] = eco_values.get_ecoflow_values()
     
-    if battery_status < 75 or power_input < 200 :
-        telegram_api.send_bot_message(battery_status, power_input)
+    if battery_status < 75 or power_input == 0 or battery_temp > 30 or battery_temp < 20 :
+        telegram_api.send_bot_message(battery_status, power_input, battery_temp)
     else:
         print('Healthy system')
--- telegram_api.py
+++ telegram_api.py
@@ -3,7 +3,7 @@ import json
 import os
 from dotenv import load_dotenv, dotenv_values 
 
-def send_bot_message(battery_status,power_input):
+def send_bot_message(battery_status,power_input, battery_temp):
 
     load_dotenv() 
     
@@ -15,7 +15,11 @@ def send_bot_message(battery_status,power_input):
     url = f"https://api.telegram.org/bot{telegram_token}/sendMessage"
 
     payload = json.dumps({
-          "text": f"⚠️ Power input: {power_input}",
+          "text": f'''
+⚠️ Power input: {power_input}
+⚠️ Battery stat: {battery_statuus}
+⚠️ Battery temp: {battery_temp}
+''',
           "chat_id": chat_id
         })
     headers = {
@@ -24,16 +28,4 @@ def send_bot_message(battery_status,power_input):
         
     response = requests.request("POST", url, headers=headers, data=payload) 
 
-    print(response.text)
-        
-    payload = json.dumps({
-          "text": f"⚠️ Battery stat: {battery_status}",
-          "chat_id": chat_id
-        })
-    headers = {
-          'Content-Type': 'application/json'
-        }
-        
-    response = requests.request("POST", url, headers=headers, data=payload)
-        
     print(response.text)
